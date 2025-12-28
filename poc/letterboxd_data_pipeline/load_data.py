dd9611d5d118c004bf1e31deb5fbc5c7a55aadbe
Darky <12773418+darkychin@users.noreply.github.com>
2025-12-28 17:47:14 +0800

    feat: add load_files_from_layer

--- poc/letterboxd_data_pipeline/load_data.py
+++ poc/letterboxd_data_pipeline/load_data.py
@@ -54,6 +54,54 @@ def load_data_from_layer(layer: str, file_type="parquet"):
     return df
 
 
+def load_files_from_layer(*, layer: str, file_list: list[str] = []):
+    """
+    Docstring for load_files_from_layer
+    
+    :param layer: data 
+    :type layer: str
+    :param file_list: Description
+    :type file_list: list[str]
+    
+    * argument design reference: https://stackoverflow.com/a/75654179/7939633
+    """
+    (layer_file_list, path) = get_file_list(layer)
+
+    target_file_list = []
+    df: dict[str, pd.DataFrame | None] = {}
+
+    if file_list:
+        target_file_list = file_list
+    else:
+        target_file_list = layer_file_list
+
+    print("Start loading data...")
+
+    for file in target_file_list:
+        """
+        List of None concatenate to avoid:
+        - IndexError: list index out of range (with file[1])
+        - ValueError: not enough values to unpack (with unpack tuple)
+
+        * reference: https://stackoverflow.com/a/21484229/7939633
+        """
+        # todo add file name clean up to avoid invalid naming for dataframe dictionary
+        (df_name, file_type) = (file.split(".") + [None])[:2]
+
+        read_path = f"{path}/{file}"
+        print(f"Loading: {read_path}")
+
+        if file_type == "csv":
+            df[df_name] = pd.read_csv(read_path)
+        else:
+            df[df_name] = pd.read_parquet(read_path)
+
+        print("Done!")
+
+    print("Finish loading.")
+    return df
+
+
 # def __main__():
 #     file_list = get_local_raw_file_list()
 
