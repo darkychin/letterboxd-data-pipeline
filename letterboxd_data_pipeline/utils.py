d6fd91e8aabc0a93f7907735e065f777d1306dc8
Darky <12773418+darkychin@users.noreply.github.com>
2026-01-15 17:54:03 +0800

    feat: add utils craw file sizes

--- letterboxd_data_pipeline/utils.py
+++ letterboxd_data_pipeline/utils.py
@@ -1,4 +1,6 @@
 import os
+from pathlib import Path
+
 
 def make_data_path(path):
     """
@@ -21,3 +23,28 @@ def compare_file_size():
     print("csv: ", csv_size)
     print("parquet: ", parquet_size)
     print("%reduce: ", (csv_size - parquet_size) / csv_size * 100)
+
+
+def crawl_all_file_size(directory: str):
+    """
+    Crawl all file sizes in the directory
+
+    :param directory: Description
+    :type directory: str
+
+    reference: https://stackoverflow.com/a/1392549/7939633
+    """
+    path = Path(directory)
+    total = 0
+
+    for file in path.glob("**/*"):
+        if file.is_file():
+            print(f"file: {file.name}")
+            file_size = file.stat().st_size
+            # ref
+            # https://stackoverflow.com/a/12912296/7939633
+            # https://stackoverflow.com/a/52684562/7939633
+            print(f"size: {file_size / (1 << 20)} MB")
+            total += file_size
+
+    print(f"total size: {total / (1 << 30)} GB")
